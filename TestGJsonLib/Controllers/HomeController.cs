28c46578d37015c7cfcac9879c9e2ceeac7e29d2
Tp10-Voda <tothp@hotmail.hu>
2020-03-19 16:00:53 +0100

    Add project files.

---
+++ TestGJsonLib/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BAMCIS.GeoJSON;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TestGJsonLib.Models;
+using TestGJsonLib.Data;
+
+namespace TestGJsonLib.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string jsonString1 = System.IO.File.ReadAllText(@"c:\projects\FIX\18JAN\cegledHelykozi.geojson");
+            //string jsonString2 = System.IO.File.ReadAllText(@"c:\projects\_GISAPP\MapSource\2g_indoor.json");
+            string jsonString3 = System.IO.File.ReadAllText(@"C:\projects\_GISAPP\MapSource\TESTGEOJSON\multiline_vasut.geojson");
+
+            //
+            //GeoJson gdata = JsonConvert.DeserializeObject<GeoJson>(jsonString1);
+            //GeoJson gdatapoly = JsonConvert.DeserializeObject<GeoJson>(jsonString2);
+
+
+            FeatureCollection Col1 = FeatureCollection.FromJson(jsonString1);
+            //FeatureCollection Col2 = FeatureCollection.FromJson(jsonString2);
+            FeatureCollection Col3 = FeatureCollection.FromJson(jsonString3);
+
+            var egyik = Col1.Features.ToList();
+            //var masik = Col2.Features.ToList();
+            var harmadik = Col3.Features.ToList();
+
+            //var sum = egyik.Concat(masik);
+
+            MyGeojson myGeojson = new MyGeojson(GeoJsonType.FeatureCollection, false, null, "tesztguid");
+            myGeojson.features = harmadik;
+            
+            //GeoJson gj = new GeoJson();
+
+            object Json = JsonConvert.SerializeObject(myGeojson);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestGJsonLib/Data/MyGeojson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BAMCIS.GeoJSON;
+
+namespace TestGJsonLib.Data
+{
+    public class MyGeojson : GeoJson
+    {
+        public string guid;
+        public List<Feature> features = new List<Feature>();
+        public MyGeojson(GeoJsonType type, bool is3D, IEnumerable<double> boundingBox, string guid) : base(type, is3D, boundingBox = null)
+        {
+            this.guid = guid;
+        }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            //return features.GetHashCode();
+            return guid.GetHashCode();
+        }
+    }
+}
